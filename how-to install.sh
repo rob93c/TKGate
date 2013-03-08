073a77d529c41a7c27d57deb3f06d8d20b022edb
rob93c <rob.uniud@gmail.com>
2013-03-08 22:19:43 +0100

    How-to install TKGate
    This script will guide you in TKGate (2-beta10)'s installation on Linux
    OS.

---
+++ how-to install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# If not autotool-dev isn't already installed you have to do:
+# sudo apt-get install autotools-dev
+sudo apt-get install build-essential autotools-dev tcl8.5-dev tk8.5-dev
+cd $HOME
+mkdir build && cd build
+# Download the package from http://www.tkgate.org/2.0/index.html and place it in $HOME
+tar xvzf tkgate-2.0-b10.tgz
+cd tkgate-2.0-b10/
+./configure
+make
+# Only if you want to install
+# sudo make install
+
+# To run TKGate:
+./src/tkgate/tkgate
